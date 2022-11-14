1a87771fc87f7603f2f04866fa5cd0de2927c33b
Wasif Raza <mwasifraza95@gmail.com>
2022-11-14 22:51:31 +0500

    classname attr error

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -3,28 +3,28 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Login</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Register</a>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Register</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Settings</a>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Settings</a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Logout</a>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Logout</a>
                 </li>
             </ul>
             </div>
