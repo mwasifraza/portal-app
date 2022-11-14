75d892209d7bb7e1901fd5fe3979d9df9832e314
Wasif Raza <mwasifraza95@gmail.com>
2022-11-14 22:52:00 +0500

    login form

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,34 @@ const Home = () => {
   return (
     <>
     <div className="container p-3">
-      <Header />
+      <div className='row'>
+        <Header />
+      </div>
+
+      <div className='row'>
+        <div className='col-sm-8'>
+          
+        </div>
+        <div className='col-sm-4 py-5 px-4'>
+          <h1 className=''>LOGIN</h1>
+          <hr />
+          <form>
+            <div className='px-4'>
+              <div className='form-floating mb-3'>
+                <input type="text" name="username" id='username' className='form-control' placeholder='Username' />
+                <label for="username">Username</label>
+              </div>
+              <div className='form-floating mb-3'>
+                <input type="text" name="password" id='password' className='form-control' placeholder='Password' />
+                <label for="password">Password</label>
+              </div>
+              <div className='d-grid'>
+                <input type="submit" name="login" value="Login" id='Login' className='btn btn-warning' />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
     </>
   )
