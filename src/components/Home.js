363de6809a3c78b72eef330ef8face4ff0dcf218
Wasif Raza <mwasifraza95@gmail.com>
2022-11-17 00:20:03 +0500

    login signup tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,18 +12,33 @@ const Home = () => {
       </div>
 
       <div className='row'>
-        <div className='col-sm-4'>
-          
-        </div>
+        <div className='col-sm-4'></div>
+        <div className='col-sm-4'></div>
+
         <div className='col-sm-4 py-5 px-4'>
-          <h1 className=''>REGISTER</h1>
-          <hr />
-          <Register />
-        </div>
-        <div className='col-sm-4 py-5 px-4'>
-          <h1 className=''>LOGIN</h1>
-          <hr />
-          <Login />
+
+          <ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+              <button class="nav-link active" id="login-tab" data-bs-toggle="tab" data-bs-target="#login-tab-pane" type="button" role="tab" aria-controls="login-tab-pane" aria-selected="true">Login</button>
+            </li>
+            <li class="nav-item" role="presentation">
+              <button class="nav-link" id="signup-tab" data-bs-toggle="tab" data-bs-target="#signup-tab-pane" type="button" role="tab" aria-controls="signup-tab-pane" aria-selected="false">Register</button>
+            </li>
+          </ul>
+
+          <div class="tab-content py-4" id="myTabContent">
+            <div class="tab-pane fade show active" id="login-tab-pane" role="tabpanel" aria-labelledby="login-tab" tabindex="0">
+              <h2 className=''>LOGIN</h2>
+              <hr />
+              <Login />
+            </div>
+            <div class="tab-pane fade" id="signup-tab-pane" role="tabpanel" aria-labelledby="signup-tab" tabindex="0">
+              <h2 className=''>REGISTER</h2>
+              <hr />
+              <Register />
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
