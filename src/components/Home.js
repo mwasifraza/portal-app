c83c9d7b3420ef39d8814ac43b45f5795351778b
Wasif Raza <mwasifraza95@gmail.com>
2022-11-18 19:11:39 +0500

    homepage styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,36 +6,31 @@ import Register from './elements/Register'
 const Home = () => {
   return (
     <>
-    <div className="container p-3">
+    <Header />
+    <div className="container">
       <div className='row'>
-        <Header />
-      </div>
+        <div className='col-sm-4 mx-auto'>
 
-      <div className='row'>
-        <div className='col-sm-4'></div>
-        <div className='col-sm-4'></div>
-
-        <div className='col-sm-4 py-5 px-4'>
-
-          <ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-              <button class="nav-link active" id="login-tab" data-bs-toggle="tab" data-bs-target="#login-tab-pane" type="button" role="tab" aria-controls="login-tab-pane" aria-selected="true">Login</button>
-            </li>
-            <li class="nav-item" role="presentation">
-              <button class="nav-link" id="signup-tab" data-bs-toggle="tab" data-bs-target="#signup-tab-pane" type="button" role="tab" aria-controls="signup-tab-pane" aria-selected="false">Register</button>
-            </li>
-          </ul>
-
-          <div class="tab-content py-4" id="myTabContent">
-            <div class="tab-pane fade show active" id="login-tab-pane" role="tabpanel" aria-labelledby="login-tab" tabindex="0">
-              <h2 className=''>LOGIN</h2>
-              <hr />
-              <Login />
-            </div>
-            <div class="tab-pane fade" id="signup-tab-pane" role="tabpanel" aria-labelledby="signup-tab" tabindex="0">
-              <h2 className=''>REGISTER</h2>
-              <hr />
-              <Register />
+          <div className='my-5 mx-4'>
+            <ul class="nav nav-tabs nav-fill" id="myTab" role="tablist">
+              <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="login-tab" data-bs-toggle="tab" data-bs-target="#login-tab-pane" type="button" role="tab" aria-controls="login-tab-pane" aria-selected="true">Login</button>
+              </li>
+              <li class="nav-item" role="presentation">
+                <button class="nav-link" id="signup-tab" data-bs-toggle="tab" data-bs-target="#signup-tab-pane" type="button" role="tab" aria-controls="signup-tab-pane" aria-selected="false">Register</button>
+              </li>
+            </ul>
+            <div class="tab-content py-4" id="myTabContent">
+              <div class="tab-pane fade show active" id="login-tab-pane" role="tabpanel" aria-labelledby="login-tab" tabindex="0">
+                <h3 className='ms-4 mb-0'>Log into account.</h3>
+                <hr />
+                <Login />
+              </div>
+              <div class="tab-pane fade" id="signup-tab-pane" role="tabpanel" aria-labelledby="signup-tab" tabindex="0">
+                <h3 className='ms-4 mb-0'>Create an account.</h3>
+                <hr />
+                <Register />
+              </div>
             </div>
           </div>
 
--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+        <nav className="navbar navbar-expand-lg navbar-bg">
+        <div className="container">
+            <a className="navbar-brand" href="#">STUDENT PORTAL</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
@@ -13,12 +13,6 @@ const Header = () => {
             <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Login</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Register</a>
                 </li>
                 <li className="nav-item">
                 <a className="nav-link" href="#">Settings</a>
