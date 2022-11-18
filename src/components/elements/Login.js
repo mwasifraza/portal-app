a159535bf5b9ae3f9189fa21f66571c49cdb7c20
Wasif Raza <mwasifraza95@gmail.com>
2022-11-19 00:55:57 +0500

    login form styling

--- src/components/elements/Login.js
+++ src/components/elements/Login.js
@@ -3,21 +3,27 @@ import React from 'react'
 const Login = () => {
   return (
     <>
-        <form>
-            <div className='px-4'>
-              <div className='form-floating mb-3'>
-                <input type="text" name="username" id='username' className='form-control' placeholder='Username' />
-                <label for="username">Username</label>
+      <form>
+          <div className='px-4 mt-4'>
+              <div className="input-group mb-3">
+                  <span className="input-group-text" id="basic-addon1"><i className="fa-solid fa-user"></i></span>
+                  <div className='float-label-text'>
+                      <input type="text" name="username" id='username' className="form-control" autoComplete='off' aria-label="Username" aria-describedby="basic-addon1" required />
+                      <label for="username">Username</label>
+                  </div>
               </div>
-              <div className='form-floating mb-3'>
-                <input type="text" name="password" id='password' className='form-control' placeholder='Password' />
-                <label for="password">Password</label>
+              <div className="input-group mb-3">
+                  <span className="input-group-text" id="basic-addon1"><i className="fa-solid fa-key"></i></span>
+                  <div className='float-label-text'>
+                      <input type="password" name="password" id='password' className="form-control" aria-label="Password" aria-describedby="basic-addon1" required/>
+                      <label for="password">Password</label>
+                  </div>
               </div>
               <div className='d-grid'>
-                <input type="submit" name="login" value="Login" id='Login' className='btn btn-warning' />
+                  <input type="submit" name="login" value="Login" id='Login' className='btn btn-warning rounded-0' />
               </div>
-            </div>
-        </form>
+          </div>
+      </form>
     </>
   )
 }
